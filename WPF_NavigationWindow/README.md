18bd3ea97f9b7981634205fdaad7567fd5d8446e
Ilya Fralkou <dzmuh@outlook.com>
2020-10-12 14:43:53 +0200

    WPF - использование навигации по страницам. NavigationWindow

---
+++ WPF_NavigationWindow/README.md
@@ -0,0 +1,6 @@
+﻿# WPF - использование навигации по страницам
+
+WPF - использование навигации по страницам, добавление контролов в NavigationWindow
+
+* <http://outcoldman.ru/ru/blog/show/149>
+* <zotero://attachment/7452/>
---
+++ WPF_NavigationWindow/WPF_NavigationWindow/ApplicationToolbar.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace WPF_NavigationWindow
+{
+    /// <summary>
+    /// Interaction logic for ApplicationToolbar.xaml
+    /// </summary>
+    public partial class ApplicationToolbar 
+    {
+        public ApplicationToolbar()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigationCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            NavigationService service = NavigationService.GetNavigationService(this);
+            service.Navigate(new Uri(e.Parameter.ToString(), UriKind.Relative));
+        }
+    }
+}
